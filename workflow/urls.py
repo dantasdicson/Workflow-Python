175b6070bc27e1193705e43cc7a98117d150019f
dantasdicson <dicksondantaz@gmail.com>
2026-03-29 12:27:29 -0300

    Move and rename freelancers/models.py to DOC/Plano de projeto.py

--- workflow/urls.py
+++ workflow/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 from rest_framework.routers import DefaultRouter
 from ordens.views import OrdemDeServicoViewSet
 from usuarios.views import UsuarioViewSet, HabilidadeViewSet
@@ -10,6 +11,7 @@ router.register(r'habilidades', HabilidadeViewSet)
 router.register(r'ordens', OrdemDeServicoViewSet)
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='api/', permanent=False)),
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
     path('api-auth/', include('rest_framework.urls')),
